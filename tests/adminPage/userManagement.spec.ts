231a7bd1039e04209f6bd5981e6af07e618ad1e4
Lhokkeesh <lhokkeeshb@gmail.com>
2026-07-28 00:14:24 +0100

    user-search-by-testcases

--- tests/adminPage/userManagement.spec.ts
+++ tests/adminPage/userManagement.spec.ts
@@ -1,7 +1,7 @@
-import {test, expect} from '../src/fixtures/page.fixtures'
-import { sideNavigation, headerTitle, dialogButtonText, createAdminOrEssUser, userRole } from '../src/test-data'
+import {test, expect} from '../../src/fixtures/page.fixtures'
+import { sideNavigation, headerTitle, dialogButtonText, createAdminOrEssUser, userRole } from '../../src/test-data'
 
-test.describe('admin tests', () => {
+test.describe('user management - admin tests', () => {
     test.beforeEach(async ({ page, dashboardPage }) => {
         await page.goto('/');
         await dashboardPage.waitFor()
@@ -64,6 +64,66 @@ test.describe('admin tests', () => {
         await expect(adminPage.userRow('asdnkmvronjxsw')).toHaveCount(0)
     })
 
+    test('search user by username', async({dashboardPage,adminPage}) => {
+        const user = createAdminOrEssUser(userRole.admin)
+        await dashboardPage.navigateThroughSideBar(sideNavigation.admin);
+        await adminPage.createUser(user);
+        await adminPage.searchUser(user.username);
+        await expect(adminPage.userRow(user.username)).toBeVisible();
+        await adminPage.deleteUserRow(user.username).click()
+        await adminPage.dialog.confirmButton(dialogButtonText.yesDelete).click()
+        await adminPage.waitFor()
+        await expect(adminPage.userRow(user.username)).toHaveCount(0)
+    })
+
+    test('search user by userrole', async({dashboardPage,adminPage}) => {
+        const user = createAdminOrEssUser(userRole.admin)
+        await dashboardPage.navigateThroughSideBar(sideNavigation.admin);
+        await adminPage.createUser(user);
+        await adminPage.searchUser(user.username);
+        await adminPage.selectDropdown('User Role', user.userRole);
+        await adminPage.searchButton.click();
+        await expect(adminPage.userRow(user.username)).toBeVisible();
+        await expect(adminPage.userRow(user.username)).toHaveCount(1)
+        await adminPage.deleteUserRow(user.username).click()
+        await adminPage.dialog.confirmButton(dialogButtonText.yesDelete).click()
+        await adminPage.waitFor()
+        await expect(adminPage.userRow(user.username)).toHaveCount(0)
+    })
+
+    test('search user by employee name', async({dashboardPage,adminPage}) => {
+        const user = createAdminOrEssUser(userRole.admin)
+        await dashboardPage.navigateThroughSideBar(sideNavigation.admin);
+        await adminPage.createUser(user);
+        await adminPage.searchUser(user.username);
+        await adminPage.employeeName.fill(user.employeeName);
+        await adminPage.waitFor()
+        await adminPage.page.waitForLoadState('domcontentloaded')
+        await adminPage.page.getByRole('option').first().click();
+        await adminPage.searchButton.click();
+        await expect(adminPage.userRow(user.username)).toBeVisible();
+        await expect(adminPage.userRow(user.username)).toHaveCount(1)
+        await adminPage.deleteUserRow(user.username).click()
+        await adminPage.dialog.confirmButton(dialogButtonText.yesDelete).click()
+        await adminPage.waitFor()
+        await expect(adminPage.userRow(user.username)).toHaveCount(0)
+    })
+
+        test('search user by status', async({dashboardPage,adminPage}) => {
+        const user = createAdminOrEssUser(userRole.admin)
+        await dashboardPage.navigateThroughSideBar(sideNavigation.admin);
+        await adminPage.createUser(user);
+        await adminPage.searchUser(user.username);
+        await adminPage.selectDropdown('Status', user.status);
+        await adminPage.searchButton.click();
+        await expect(adminPage.userRow(user.username)).toBeVisible();
+        await expect(adminPage.userRow(user.username)).toHaveCount(1)
+        await adminPage.deleteUserRow(user.username).click()
+        await adminPage.dialog.confirmButton(dialogButtonText.yesDelete).click()
+        await adminPage.waitFor()
+        await expect(adminPage.userRow(user.username)).toHaveCount(0)
+    })
+
     test('edit user', async({dashboardPage,adminPage}) => {
         await dashboardPage.navigateThroughSideBar(sideNavigation.admin);
         const user = createAdminOrEssUser(userRole.admin)
@@ -74,10 +134,11 @@ test.describe('admin tests', () => {
         await expect(adminPage.userRow(user.username)).toHaveCount(1)
         await adminPage.editUser(user.username, {username: updatedUserName})
         await adminPage.searchUser(updatedUserName);
+        await adminPage.waitFor()
         await expect(adminPage.userRow(updatedUserName)).toBeVisible();
         await adminPage.deleteUserRow(updatedUserName).click();
         await adminPage.dialog.confirmButton(dialogButtonText.yesDelete).click()
         await adminPage.searchUser(updatedUserName);
         await expect(adminPage.userRow(updatedUserName)).toHaveCount(0);
-    })
+          })
 }) 
